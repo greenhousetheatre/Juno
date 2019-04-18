a4eba08b4775eb08483cf1f762ca2a3665df9ddc
Sepharial <mjf22@st-andrews.ac.uk>
2019-04-18 14:52:03 +0100

    Hm

--- sendmail.php
+++ sendmail.php
@@ -1,57 +1,56 @@
 <?php
-
 if(!isset($_POST['submit']))
 {
-	echo "Error: Please submit form!"
+	echo "Error: Please submit form!";
 }
-
 $visitor_name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $visitor_subject = $_POST['message'];
 
-if(empty($visitor_name)||empty($visitor_email))
-	{	
-		echo "Error: Please enter your name and email address!"
-	}
+//Validate first
+if(empty($visitor_name)||empty($visitor_email)) 
+{
+    echo "Error: Please enter your name and email address!";
+    exit;
+}
 
-$email_from = 'ofgalaxies@gmail.com';
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = "ofgalaxies@gmail.com";
 $email_subject = "Message from $visitor_name for BoxedIn Theatre";
 $email_body = "You have received a new message from $visitor_name, $visitor_email.\n".
 	"Here is the message:\n $visitor_subject".
-
-$to = "ofgalaxies@gmail.com";
+    
+$to = "tom@amazing-designs.com";
 $headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 header('Location: thankyou.html');
 
 function IsInjected($str)
-	{
-    	$injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-    	$inject = join('|', $injections);
-    	$inject = "/$inject/i";
-     
-    	if(preg_match($inject,$str))
-    		{
-      			return true;
-    		}
-    	else
-    		{
-      			return false;
-    		}
-	}
-
-	if(IsInjected($visitor_email))
-		{
-    		echo "NO!";
-    		exit;
-		}
-
-?>
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
